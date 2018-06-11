a7b90f5b4d6eb1eda49d9f0d1c8a45de92cb08f2
macir <mesut.ozturk@bsdmail.com>
2018-06-11 12:38:22 +0300

    First Commit

---
+++ scripts/integration.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+source .envrc
+./scripts/install_tools.sh
+
+GINKGO_NODES=${GINKGO_NODES:-3}
+GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-1}
+export CF_STACK=${CF_STACK:-cflinuxfs2}
+
+cd src/*/integration
+
+echo "Run Uncached Buildpack"
+ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=120 -- --cached=false
+
+echo "Run Cached Buildpack"
+ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=120 -- --cached
